5ce8fbd3eda4716a7c727575d3b525e909dc8ce6
Tanishq Sandhu <60661385+tanishqsandhu@users.noreply.github.com>
2026-06-11 16:46:47 -0500

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,10 @@
 import type { Config } from 'tailwindcss'
 const config: Config = {
-  content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './node_modules/react-tailwindcss-datepicker/dist/index.esm.js',
+  ],
   theme: { extend: {} },
   plugins: [],
 }
